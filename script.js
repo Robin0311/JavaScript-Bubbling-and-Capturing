1d532840fc56551c29f5881c32b0e679b2d3d51c
Robin1103 <robinson.9203@gmail.com>
2022-05-15 15:24:22 -0700

    actualizando

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const btn = document.querySelector('button');
-const videoBox = document.querySelector('div');
+const videoBox = document.querySelector('div#videoBox');
 const video = document.querySelector('video');
 
 btn.onclick = function() {
@@ -10,6 +10,27 @@ videoBox.onclick = function() {
   videoBox.setAttribute('class','hidden');
 };
 
-video.onclick = function() {
+video.onclick = function(event) {
+  event.stopPropagation();//sirve para hacer que el video no se esconda
   video.play();
 };
+
+const firstValue = document.querySelector('input.firstValue')
+const secondValue = document.querySelector('input.secondValue')
+const buttons = document.querySelector('#buttons')
+const result = document.querySelector('#result')
+
+buttons.onclick = function(e) {
+  switch (e.target.id) {
+    case 'add':
+      result.textContent = Number(firstValue.value) + Number(secondValue.value);
+      break;
+    case 'rest':
+      result.textContent = firstValue.value - secondValue.value;
+      break;
+    case 'multiply':
+      result.textContent = firstValue.value * secondValue.value;
+      break;
+  }
+}
+
